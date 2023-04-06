746b034080a53a7767e4b33be3311daa65489161
patztablook22 <patz@tuta.io>
2023-04-06 20:03:10 +0100

    timer

--- media/js/main.js
+++ media/js/main.js
@@ -8,12 +8,17 @@ const main = () => {
     image.classList.add("to-reveal");
     pseudonym.classList.add("to-reveal");
 
+    let counter = 0;
+
     const reveal = () => {
+        if (++counter < 2) return;
+
         image.classList.add("revealed");
         pseudonym.classList.add("revealed");
     }
 
     image.onload = reveal;
+    setInterval(reveal, 500);
 }
 
 document.addEventListener("DOMContentLoaded", main);
